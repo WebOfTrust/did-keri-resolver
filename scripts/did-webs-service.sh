d53b4086e95399ef29eb4bee46fcc374b4729ef7
Markus Sabadello <markus@danubetech.com>
2023-09-06 23:53:11 +0200

    Remove web server key/cert arguments

--- scripts/did-webs-service.sh
+++ scripts/did-webs-service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dkr did webs service --config-dir=./scripts --keypath=/usr/local/var/certs/privkey1.pem --certpath=/usr/local/var/certs/cert1.pem --cafilepath=/usr/local/var/certs/chain1.pem
+dkr did webs service --config-dir=./scripts
